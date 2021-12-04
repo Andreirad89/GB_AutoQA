b0d220e32e544135566e7e071f2c725733abb1c5
Andreirad89 <andreirad89@gmail.com>
2021-12-04 22:38:30 +0300

    homework4triangle+testTreangleArea

---
+++ WorkHome2_architype/src/test/java/org/example/TreangleTest.java
@@ -0,0 +1,22 @@
+package org.example;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import workhome4.Triangle;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TreangleTest {
+    @Test
+    void egyptTriangleThenSuccesedPerimetrTest() {
+        Triangle triangle=new Triangle(3,4,5); //готовим данные
+        int actualResult = triangle.calculatePerimetr(); //вызыввем метод который хотим проверить
+        assertEquals(12,actualResult); // сравниваем желаемый результат с актуальным
+    }
+
+    void checkAreaTriangle() {
+        Triangle triangle=new Triangle(3,4,5); //готовим данные
+        double actualResult = triangle.areaTriangle(); //вызыввем метод который хотим проверить
+        assertEquals(6,actualResult); // сравниваем желаемый результат с актуальным
+    }
+}
